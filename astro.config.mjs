a4a75306779ea5251fae4491efbba9b4a32cd175
nilsreich <nilsreich@gmail.com>
2025-05-13 22:14:39 +0000

    Add new images and implement PWA support
    - Added new image assets: fensterputzer.avif, fussball.avif, koch.avif, and tanzen.avif.
    - Updated TypeScript environment definitions in env.d.ts for PWA support.
    - Imported Tailwind CSS in global.css for styling.
    - Implemented service worker registration in pwa.ts for offline capabilities.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,47 +1,43 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import AstroPWA from '@vite-pwa/astro'
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), AstroPWA({
- registerType: 'autoUpdate',
-        outDir: 'dist', // Das Verzeichnis, in das Astro baut
-        manifest: {
-          name: 'Offline Bilder App',
-          short_name: 'OfflineBilder',
-          theme_color: '#262626', // Dein Hintergrundfarbe
-          icons: [
-            // Hier definierst du Icons für die PWA
-            // {
-            //   src: 'pwa-192x192.png',
-            //   sizes: '192x192',
-            //   type: 'image/png'
-            // },
-            // {
-            //   src: 'pwa-512x512.png',
-            //   sizes: '512x512',
-            //   type: 'image/png'
-            // }
-          ],
+  integrations: [AstroPWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Mein React template',
+      short_name: 'ReactTemplate',
+      description: 'A simple React template application.',
+      theme_color: '#ffffff', // Light theme primary color
+      background_color: '#f3f4f6', // Light theme secondary color
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+      icons: [
+        {
+          src: 'icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
         },
-        workbox: {
-          // Hier konfigurierst du das Caching
-          globDirectory: 'dist', // Das Verzeichnis, das durchsucht wird
-          globPatterns: [
-            '**/*.{html,css,js,ico,png,svg,avif}', // Cache HTML, CSS, JS und alle relevanten Bildformate
-          ],
-          // Füge weitere Caching-Strategien hinzu, falls nötig
-        },
-        devOptions: {
-            enabled: true // PWA auch im Entwicklungsmodus testen
+        {
+          src: 'icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
         }
-      })],
+      ],
+    },
+    workbox: {
+      globPatterns: [
+        '**/*.{html,css,js,ico,png,svg,avif}',
+      ],
+    },
+    devOptions: {
+      enabled: true 
+    },
+    
+  })],
   vite: {
     plugins: [tailwindcss()]
   }
